82e277f4677f674bc3cea51c095a8fcbf6255c35
folatkom <folatko.monika@gmail.com>
2024-10-24 23:20:05 +0200

    feat: add controlled form

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -3,8 +3,34 @@ import FormInput from "../../components/formInput";
 import Heading from "../../components/heading";
 import Text from "../../components/text";
 import contactModel from "../../models/contact.model";
+import { useState } from "react";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    service: "",
+  });
+
+  const submitMessage = `Nice to meet you ${formData.firstName} ${formData.lastName}!\nWe're thrilled to have a chat with you about ${formData.service}.\nWe will reach you on ${formData.phone} within 24 hours.`;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(submitMessage);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      phone: "",
+      service: "",
+    });
+  };
+
   return (
     <div className="contact">
       <div className="container">
@@ -13,26 +39,39 @@ const Contact = () => {
           <Text>{contactModel.text}</Text>
         </div>
 
-        <div>
+        <form name="form" onSubmit={handleSubmit}>
           <div className="row">
-            <FormInput placeholder="First Name" />
-            <FormInput placeholder="Last Name" />
+            <FormInput
+              name="firstName"
+              placeholder="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+            <FormInput
+              name="lastName"
+              placeholder="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
           </div>
-          <FormInput placeholder="Phone Number" style="full" />
           <FormInput
+            name="phone"
+            placeholder="Phone Number"
+            style="full"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+          <FormInput
+            name="service"
             placeholder="What Service are you interested in?"
             style="full"
+            value={formData.service}
+            onChange={handleChange}
           />
-          <button
-            type="submit"
-            className="btn"
-            onClick={() => {
-              console.log("clicked");
-            }}
-          >
+          <button type="submit" className="btn">
             SUBMIT NOW
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
